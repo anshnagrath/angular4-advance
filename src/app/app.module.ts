13db38da6caf68ba3b397feee33443bf95eb77d9
anshnagrath <anshnagrath448@gmail.com>
2018-05-22 21:27:32 +0530

    firdt view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-
-
+import {AllregionComponent} from './region/allregion/allregion.component';
+import {RegionModule} from './region/region.module';
+import { AppserviceService } from './appservice.service'; 
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RegionModule,
+    ToastModule.forRoot(),
+    RouterModule.forRoot([
+      {path: 'region', component:AllregionComponent, pathMatch:'full'},
+      {path: '', redirectTo:'region', pathMatch:'full' },
+      {path: '*', component:AllregionComponent},
+      {path: '**', component:AllregionComponent },
+    ])
   ],
-  providers: [],
+  providers: [AppserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
